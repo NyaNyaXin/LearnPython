4208cb9c9f006eb1381446aa62c40cad3c7ed6e0
NyaNyaXin <chenxin95013>
2017-07-29 16:42:16 +0800

    Day01CallFunction

---
+++ CallFunction.py
@@ -0,0 +1,19 @@
+#调用函数
+print(abs(10))
+print(abs(-1))
+#错误情况
+#1.参数个数不对
+# abs(11,2)
+#2.参数类型不正确
+# abs('2')
+
+#数据类型转换
+# print(int('123'))
+# print(int(12.34))
+# print(str(1.23))
+# print(str(100))
+# print(bool(2))
+# print(bool(''))
+
+#整数转换成十六进制数
+print(hex(15))
